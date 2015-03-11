a8319a2594bf660bd8db0c5fc4cc24534cad9026
VivekShankar <vshanka1@andrew.cmu.edu>
2015-03-11 15:16:57 -0400

    Added raga database functions into ragas.py

--- ragas.py
+++ ragas.py
@@ -4,8 +4,10 @@ from scipy.fftpack import fft
 from pylab import *
 import os
 import math
+import contextlib # for urllib.urlopen()
+import urllib
+import os
 #from pydub import AudioSegment
-
 #file1 = AudioSegment.from_wav('ri.wav')
 #file2 = AudioSegment.from_wav('sa.wav')
 #song = file1[:250] + file2[:500]
@@ -39,13 +41,40 @@ import math
 # plt.plot(fAxis,abs(c[:(d-1)]),'r') 
 # plt.show()
 
+def readFile(filename, mode="rt"):
+    # rt = "read text"
+    with open(filename, mode) as fin:
+        return fin.read()
+
+def addRagasToDict(textfile):
+    #returns dictionary
+    #maps raga name to list containing its constituent notes (only Arohanam)
+    ragaDict = dict()
+    text = readFile(textfile)
+    ragaList = text.splitlines()
+    for raga in ragaList:
+        nameStartIndex = raga.index("|")
+        nameEndIndex = raga.index("|",nameStartIndex+1)
+        name = raga[nameStartIndex+1:nameEndIndex].strip()
+        notes = raga[nameEndIndex+1:].strip()
+        notesList = notes.split()
+        #G1 should become R2
+        #N1 should become D2
+        for i in xrange(len(notesList)):
+            if notesList[i] == 'G1':
+                notesList[i] = 'R2'
+            elif notesList[i] == 'N1':
+                notesList[i] = 'D2'
+        ragaDict[name] = notesList
+    return ragaDict
+
 def frequencyToNote(freq):
     lowSa = 636
     a = 1.057994353 #factor to get to new notes
     k = math.log(freq*1.0/lowSa, a)
     k = int(round(k))
-    notesList = (["Sa", "Ri1", "Ri2", "Ga1", "Ga2", 
-    "Ma1", "Ma2", "Pa", "Da1", "Da2", "Ni1", "Ni2"])
+    notesList = (["S", "R1", "R2", "G2", "G3", 
+    "M1", "M2", "P", "D1", "D2", "N2", "N3"])
     return notesList[k%12]
     
 def windowFunction(n):
@@ -62,7 +91,33 @@ def isRagam(notesList, thresholdPercentage, ragam):
             numRagam += 1
     percentageMohanam = numRagam*1.0/len(notesList)
     return percentageMohanam >= thresholdPercentage
+    
+def isRagam2(notesList, thresholdPercentage, ragam):
+    #takes in a list of notes, thresholdPercentage, ragam name (string)
+    #to determine whether a list of notes is a particular ragam
+    ragaDict = addRagasToDict("RagaDatabase.txt")
+    ragaNotes = ragaDict[ragam] #raga arohanam"
+    numRagam = 0
+    for note in notesList:
+        if note in ragaNotes:
+            numRagam += 1
+    percentageRagam = numRagam*1.0/len(notesList)
+    return percentageRagam >= thresholdPercentage
 
-#mohanam = ["Sa", "Ri2", "Ga2", "Pa", "Da2"]
-#madhyamavathi = ["Sa", "Ri2", "Ma1", "Pa", "Ni2"]
-#hindolam = ["Sa", "Ga1", "Ma1", "Da1", "Ni1"]
+def findposRagams(notesList, thresholdPercentage):
+    ragaDict = addRagasToDict("/Users/vivekshankar/Documents/Raga Identifying Program/RagaDatabase.txt")
+    posRagas = []
+    for ragam in ragaDict:
+        ragaNotes = ragaDict[ragam]
+        numRagam = 0
+        for note in notesList:
+            if note in ragaNotes:
+                numRagam += 1
+        percentageRagam = numRagam*1.0/len(notesList)
+        if percentageRagam >= thresholdPercentage:
+            posRagas += [ragam]
+    return posRagas
+
+#mohanam =  ["S", "R2", "G3", "P", "D2"]
+#madhyamavathi = ["S", "R2", "M1", "P", "N2"]
+#hindolam = ["S", "G1", "M1", "D1", "N1"]
